d3b5d5c643d350bd408f623bea1c8c9cc8b070ef
gaoyuan tan <chain_dev@163.com>
2020-06-15 18:22:23 +0800

    update

--- cosmos/headerverify_test.go
+++ cosmos/headerverify_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestVrifyHeder(t *testing.T) {
+func TestVerifyHeader(t *testing.T) {
 	chainId := "testing"
 	height := int64(100)
 	c := NewHTTPClient()
@@ -32,7 +32,7 @@ func TestVrifyHeder(t *testing.T) {
 	fmt.Printf("verify header successful!")
 }
 
-func TestVrifyHeder1(t *testing.T) {
+func TestVerifyHeader1(t *testing.T) {
 	chainId := "testing"
 	height := int64(100)
 	c := NewHTTPClient()
@@ -55,7 +55,7 @@ func TestVrifyHeder1(t *testing.T) {
 	fmt.Printf("verify header successful!")
 }
 
-func TestVrifyHeder2(t *testing.T) {
+func TestVerifyHeader2(t *testing.T) {
 	chainId := "testing"
 	height := int64(100)
 	c := NewHTTPClient()
@@ -72,5 +72,54 @@ func TestVrifyHeder2(t *testing.T) {
 		panic(err)
 	}
 	fmt.Printf("verify header successful!")
+}
+
+func TestVerifyHeader3(t *testing.T) {
+	chainId := "testing"
+	height := int64(100)
+	c := NewHTTPClient()
+	preBlock := GetBlock(c, height - 1)
+	validatorSet := GetValidatorSet(c, height)
+	valset := types.NewValidatorSet(validatorSet)
+	signedHeader := GetSignedHeader(c, height)
+	if bytes.Equal(preBlock.NextValidatorsHash, valset.Hash()) != true {
+		panic("next validator hash is not right!\n")
+	}
+	if bytes.Equal(signedHeader.Header.ValidatorsHash, valset.Hash()) != true {
+		panic("validator hash is not right!\n")
+	}
+	if signedHeader.Commit.GetHeight() != signedHeader.Height {
+		panic("commit height is not right!\n")
+	}
+	if bytes.Equal(signedHeader.Commit.BlockID.Hash, signedHeader.Header.Hash()) != true {
+		panic("commit hash is not right!\n")
+	}
+	if err := signedHeader.Commit.ValidateBasic(); err != nil {
+		panic(err)
+	}
+	if valset.Size() != len(signedHeader.Commit.Signatures) {
+		panic("the size of precommits is not right!")
+	}
+	talliedVotingPower := int64(0)
+	for idx, commitSig := range signedHeader.Commit.Signatures {
+		if commitSig.Absent() {
+			continue // OK, some precommits can be missing.
+		}
+		_, val := valset.GetByIndex(idx)
+		// Validate signature.
+		precommitSignBytes := signedHeader.Commit.VoteSignBytes(chainId, idx)
+		if !val.PubKey.VerifyBytes(precommitSignBytes, commitSig.Signature) {
+			panic("Invalid commit -- invalid signature!")
+		}
+		// Good precommit!
+		if signedHeader.Commit.BlockID.Equals(commitSig.BlockID(signedHeader.Commit.BlockID)) {
+			talliedVotingPower += val.VotingPower
+		}
+	}
+
+	if talliedVotingPower <= valset.TotalVotingPower()*2/3 {
+		panic("voteing power is not enough!")
+	}
+	fmt.Printf("verify header successful!\n")
 }
 

61fa2165387c9c49a0b7d6ca7d6730f83b7868b4
gaoyuan tan <chain_dev@163.com>
2020-07-31 18:03:52 +0800

    update

--- usage/ontology/account_test.go
+++ usage/ontology/account_test.go
@@ -9,7 +9,7 @@ import (
 func TestOntologyBalance1(t *testing.T) {
 	// create alliance sdk
 	ontSdk := newOntologySdk()
-	account_user, _ := common.AddressFromBase58(LAYER2_CONTRACT)
+	account_user, _ := common.AddressFromHexString("f05db35f619a991435b160b21e4d9371fdc0d9f2")
 	//
 	balance := getOntologyOngBalance(ontSdk, account_user)
 	fmt.Printf("amount of address %s is: %d\n", account_user.ToBase58(), balance)

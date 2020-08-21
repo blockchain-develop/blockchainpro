4e4f2d47cc1b9ce3ddfcbe211dd82d0f6bc9a9d8
gaoyuan tan <chain_dev@163.com>
2020-08-21 11:41:47 +0800

    update

--- usage/layer2/layer2_transfer_ft_test.go
+++ usage/layer2/layer2_transfer_ft_test.go
@@ -64,56 +64,6 @@ func TestLayer2TransferOng_ft(t *testing.T) {
 	fmt.Printf("amount of address %s is: %d\n", account_user.Address.ToBase58(), balance)
 }
 
-func TestLayer2OngBalance_ft(t *testing.T) {
-	// create alliance sdk
-	layer2_sdk := newLayer2Sdk()
-	account_user, _ := newLayer2UserAccount(layer2_sdk)
-	//
-	balance := getLayer2OngBalance(layer2_sdk, account_user.Address)
-	fmt.Printf("amount of address %s is: %d\n", account_user.Address.ToBase58(), balance)
-}
-
-func TestLayer2OntBalance_ft(t *testing.T) {
-	// create alliance sdk
-	layer2_sdk := newLayer2Sdk()
-	account_user, _ := newLayer2UserAccount(layer2_sdk)
-	//
-	balance := getLayer2OntBalance(layer2_sdk, account_user.Address)
-	fmt.Printf("amount of address %s is: %d\n", account_user.Address.ToBase58(), balance)
-}
-
-func TestGetLayer2CurrentHeight_ft(t *testing.T) {
-	layer2_sdk := newLayer2Sdk()
-	height, err := layer2_sdk.GetCurrentBlockHeight()
-	if err != nil {
-		fmt.Printf("get current block height err: %s", err.Error())
-	}
-	fmt.Printf("current block height: %d\n", height)
-}
-
-func TestGetLayer2Block_ft(t *testing.T) {
-	layer2_sdk := newLayer2Sdk()
-	block, err := layer2_sdk.GetLayer2BlockByHeight(20)
-	if err != nil {
-		fmt.Printf("get current block height err: %s", err.Error())
-	}
-	fmt.Printf("get current block successful!, State root hash: %s\n", block.Header.StateRoot.ToHexString())
-}
-
-func TestGetLayer2Transaction_ft(t *testing.T) {
-	layer2Sdk := newLayer2Sdk()
-	txHash := ""
-	tx, err := layer2Sdk.GetTransaction(txHash)
-	if err != nil {
-		panic(err)
-	}
-	if tx == nil {
-		panic("this is no this tx!")
-	}
-	getTxHash := tx.Hash()
-	fmt.Printf("tx hash is: %s\n", getTxHash.ToHexString())
-}
-
 func TestGetCommitedLayer2StateByHeight_ft(t *testing.T) {
 	//
 	ontSdk := newOntologySdk()

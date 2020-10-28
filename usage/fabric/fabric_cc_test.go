46044b27be6a7ef4e4cdb451484c86db47e05cb5
gaoyuan tan <chain_dev@163.com>
2020-10-28 17:27:32 +0800

    update

--- usage/fabric/fabric_cc_test.go
+++ usage/fabric/fabric_cc_test.go
@@ -3,11 +3,13 @@ package fabric
 import (
 	"fmt"
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/event"
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/resmgmt"
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/errors/retry"
 	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
 	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
 	"testing"
+	"time"
 )
 
 func newFabSdk() *fabsdk.FabricSDK {
@@ -34,6 +36,15 @@ func newChannelClient(sdk *fabsdk.FabricSDK) *channel.Client {
 		panic(err)
 	}
 	return cc
+}
+
+func newEventClient(sdk *fabsdk.FabricSDK) *event.Client {
+	ccp := sdk.ChannelContext("mychannel", fabsdk.WithUser("User1"), fabsdk.WithOrg("Org1"))
+	eventClient, err := event.New(ccp)
+	if err != nil {
+		panic(err)
+	}
+	return eventClient
 }
 
 func packArgs(args []string) [][]byte {
@@ -72,4 +83,35 @@ func TestCCInvoke(t *testing.T) {
 		panic(err)
 	}
 	fmt.Printf("response: %v\n", string(response.TransactionID))
+}
+
+func TestCCEvent(t *testing.T) {
+	sdk := newFabSdk()
+	channelClient := newChannelClient(sdk)
+	eventClient := newEventClient(sdk)
+
+	eventID := "([a-zA-Z]+)"
+	reg, notifier, err := eventClient.RegisterChaincodeEvent("basic", eventID)
+	if err != nil {
+		panic(err)
+	}
+	defer eventClient.Unregister(reg)
+
+	req := channel.Request{
+		ChaincodeID: "basic",
+		Fcn: "TransferAsset",
+		Args: packArgs([]string{"asset6","Christopher"}),
+	}
+	response, err := channelClient.Execute(req, channel.WithRetry(retry.DefaultChannelOpts))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("response: %s\n", string(response.Payload))
+
+	select {
+	case ccEvent := <- notifier:
+		fmt.Printf("receive cc event:%v\n", ccEvent)
+	case <- time.After(time.Second * 60):
+		fmt.Printf("not receive cc event!")
+	}
 }
